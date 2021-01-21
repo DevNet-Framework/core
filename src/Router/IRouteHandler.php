9f42b443e15160e56d3c35ff794f435f2a7f9387
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-01-21 14:37:49 -0500

    update IRouteHandler add __set() method

--- src/Router/IRouteHandler.php
+++ src/Router/IRouteHandler.php
@@ -12,5 +12,11 @@ use Artister\System\Process\Task;
 
 interface IRouteHandler
 {
+    /**
+     * This method must set the value of following properity
+     * @var mixed $Target (represent the endpoit request handler)
+     */
+    public function __set(string $name, $value);
+
     public function handle(RouteContext $routeContext) : Task;
 }

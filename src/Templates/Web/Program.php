c6da03be680963e83466d4dd9ca01c5fca8c0095
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-02-27 00:56:45 -0500

    update  web project template

--- src/Templates/Web/Program.php
+++ src/Templates/Web/Program.php
@@ -23,61 +23,62 @@ class Program
         $namespace  = "Application";
 
         $parser = new CommandParser();
-        $parser->addOption('--name');
-        $parser->addOption('--directory');
+        $parser->addOption('--main');
+        $parser->addOption('--project');
         $arguments = $parser->parse($args);
 
-        $nameOption = $arguments->getOption('--name');
-        if ($nameOption) {
+        $nameOption = $arguments->getOption('--main');
+        if ($nameOption)
+        {
             $className = $nameOption->Value;
         }
 
-        if (!$className) {
+        if (!$className)
+        {
             Console::foregroundColor(ConsoleColor::Red);
             Console::writeline("class Name not found, maybe forget to enter class name using the option --main");
             Console::resetColor();
             exit;
         }
 
-        $directoryOption = $arguments->getOption('--directory');
-        if ($directoryOption) {
-            $basePath = $directoryOption->Value;
+        $projectOption = $arguments->getOption('--project');
+        if ($projectOption)
+        {
+            $basePath = $projectOption->Value;
         }
 
         $path = implode("/", [$rootPath, $basePath]);
 
-        if (!is_dir($path)) {
-            Console::foregroundColor(ConsoleColor::Red);
-            Console::writeline("Invalid Path $path");
-            Console::resetColor();
-            exit;
+        if (!is_dir($path))
+        {
+            mkdir($path, 0777, true);
         }
 
         $result = self::createProgram($path, $namespace, $className);
 
         if ($result)
         {
-            self::copyFile( __DIR__.'/resources/project.phproj', $rootPath."/project.phproj");
-            self::copyFile( __DIR__.'/resources/composer.json', $rootPath."/composer.json");
-            self::copyFile( __DIR__.'/resources/Startup.php', $rootPath."/Startup.php");
-            self::copyFile( __DIR__.'/resources/Routes.php', $rootPath."/Routes.php");
-            self::copyFile( __DIR__.'/resources/settings.json', $rootPath."/settings.json");
-            self::copyFile( __DIR__.'/resources/Controllers/HomeController.php', $rootPath."/Controllers/HomeController.php");
-            self::copyFile( __DIR__.'/resources/Controllers/AccountController.php', $rootPath."/Controllers/AccountController.php");
-            self::copyFile( __DIR__.'/resources/Models/LoginForm.php', $rootPath."/Models/LoginForm.php");
-            self::copyFile( __DIR__.'/resources/Views/Home/index.phtml', $rootPath."/Views/home/index.phtml");
-            self::copyFile( __DIR__.'/resources/Views/Account/index.phtml', $rootPath."/Views/Account/index.phtml");
-            self::copyFile( __DIR__.'/resources/Views/Account/login.phtml', $rootPath."/Views/Account/login.phtml");
-            self::copyFile( __DIR__.'/resources/Views/Account/register.phtml', $rootPath."/Views/Account/register.phtml");
-            self::copyFile( __DIR__.'/resources/Views/Layouts/layout.phtml', $rootPath."/Views/Layouts/layout.phtml");
-            self::copyFile( __DIR__.'/resources/Views/Layouts/navbar.phtml', $rootPath."/Views/Layouts/navbar.phtml");
-            self::copyFile( __DIR__.'/resources/webroot/css/style.css', $rootPath."/webroot/css/style.css");
-            self::copyFile( __DIR__.'/resources/webroot/lib/bootstrap/css/bootstrap.min.css', $rootPath."/webroot/lib/bootstrap/css/bootstrap.min.css");
-            self::copyFile( __DIR__.'/resources/webroot/js/script.js', $rootPath."/webroot/js/script.js");
-            self::copyFile( __DIR__.'/resources/webroot/lib/bootstrap/js/bootstrap.bundle.min.js', $rootPath."/webroot/lib/bootstrap/js/bootstrap.bundle.min.js");
-            self::copyFile( __DIR__.'/resources/webroot/index.php', $rootPath."/webroot/index.php");
-            self::copyFile( __DIR__.'/resources/webroot/web.config', $rootPath."/webroot/web.config");
-            self::copyFile( __DIR__.'/resources/webroot/.htaccess', $rootPath."/webroot/.htaccess");
+            self::copyFile( __DIR__.'/resources/project.phproj', $path."/project.phproj");
+            self::copyFile( __DIR__.'/resources/composer.json', $path."/composer.json");
+            self::copyFile( __DIR__.'/resources/Startup.php', $path."/Startup.php");
+            self::copyFile( __DIR__.'/resources/Routes.php', $path."/Routes.php");
+            self::copyFile( __DIR__.'/resources/settings.json', $path."/settings.json");
+            self::copyFile( __DIR__.'/resources/Controllers/HomeController.php', $path."/Controllers/HomeController.php");
+            self::copyFile( __DIR__.'/resources/Controllers/AccountController.php', $path."/Controllers/AccountController.php");
+            self::copyFile( __DIR__.'/resources/Models/LoginForm.php', $path."/Models/LoginForm.php");
+            self::copyFile( __DIR__.'/resources/Views/Home/index.phtml', $path."/Views/home/index.phtml");
+            self::copyFile( __DIR__.'/resources/Views/Account/index.phtml', $path."/Views/Account/index.phtml");
+            self::copyFile( __DIR__.'/resources/Views/Account/login.phtml', $path."/Views/Account/login.phtml");
+            self::copyFile( __DIR__.'/resources/Views/Account/register.phtml', $path."/Views/Account/register.phtml");
+            self::copyFile( __DIR__.'/resources/Views/Layouts/layout.phtml', $path."/Views/Layouts/layout.phtml");
+            self::copyFile( __DIR__.'/resources/Views/Layouts/navbar.phtml', $path."/Views/Layouts/navbar.phtml");
+            self::copyFile( __DIR__.'/resources/webroot/css/style.css', $path."/webroot/css/style.css");
+            self::copyFile( __DIR__.'/resources/webroot/lib/bootstrap/css/bootstrap.min.css', $path."/webroot/lib/bootstrap/css/bootstrap.min.css");
+            self::copyFile( __DIR__.'/resources/webroot/js/script.js', $path."/webroot/js/script.js");
+            self::copyFile( __DIR__.'/resources/webroot/lib/bootstrap/js/bootstrap.bundle.min.js', $path."/webroot/lib/bootstrap/js/bootstrap.bundle.min.js");
+            self::copyFile( __DIR__.'/resources/webroot/index.php', $path."/webroot/index.php");
+            self::copyFile( __DIR__.'/resources/webroot/web.config', $path."/webroot/web.config");
+            self::copyFile( __DIR__.'/resources/webroot/.htaccess', $path."/webroot/.htaccess");
 
             Console::foregroundColor(ConsoleColor::Green);
             Console::writeline("The template 'Web Application' was created successfully.");
@@ -88,8 +89,8 @@ class Program
 
     public static function createProgram($path, $namespace, $className) : bool
     {
-        $namespace  = ucwords($namespace, "\\");
-        $className  = ucfirst($className);
+        $namespace = ucwords($namespace, "\\");
+        $className = ucfirst($className);
 
         $context = new StringBuilder();
         $context->appendLine("<?php");
@@ -113,11 +114,12 @@ class Program
         $context->appendLine("    }");
         $context->append("}");
 
-        $myfile     = fopen($path."/".$className.".php", "w");
-        $size       = fwrite($myfile, $context->__toString());
-        $status     = fclose($myfile);
+        $myfile = fopen($path."/".$className.".php", "w");
+        $size   = fwrite($myfile, $context->__toString());
+        $status = fclose($myfile);
 
-        if ($size && $status) {
+        if ($size && $status)
+        {
             return true;
         }
 
@@ -126,12 +128,14 @@ class Program
 
     public static function copyFile($srcfile, $dstfile)
     {
-        $desrir =  dirname($dstfile);
-        if (!is_dir($desrir)) {
-            mkdir($desrir, 0777, true);
+        $dstdir =  dirname($dstfile);
+        if (!is_dir($dstdir))
+        {
+            mkdir($dstdir, 0777, true);
         }
 
-        if (!file_exists($dstfile)) {
+        if (!file_exists($dstfile))
+        {
             copy($srcfile, $dstfile);
         }
     }

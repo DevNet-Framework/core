5e40a621f34ea0a6824acb2f7a64d43d5ce1e1de
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-03-24 00:30:09 -0400

    update ServiceCollectionExtensions

--- src/Extensions/ServiceCollectionExtensions.php
+++ src/Extensions/ServiceCollectionExtensions.php
@@ -36,28 +36,17 @@ use Closure;
 
 class ServiceCollectionExtensions
 {
-    public static function addHttpContext(IServiceCollection $service)
-    {
-        $service->addSingleton(HttpContext::class, function($provider) : HttpContext {
-            $httpContext = HttpContextFactory::create();
-            $httpContext->addAttribute('RequestServices', $provider);
-            return $httpContext;
-        });
-    }
-
-    public static function addAntiforgery(IServiceCollection $service, Closure $configuration = null)
-    {
-        $options = new AntiforgeryOptions();
+    public static function addAntiforgery(IServiceCollection $services, $options = new AntiforgeryOptions();
 
         if ($configuration)
         {
             $configuration($options);
         }
 
-        $service->addSingleton(IAntiforgery::class, fn() => new Antiforgery($options));
+        $services->addSingleton(IAntiforgery::class, fn() => new Antiforgery($options));
     }
 
-    public static function addAuthentication(IServiceCollection $service, Closure $configuration = null)
+    public static function addAuthentication(IServiceCollection $services, Closure $configuration = null)
     {
         $builder = new AuthenticationBuilder();
         $builder->addCookie(AuthenticationDefaults::AuthenticationScheme);
@@ -67,10 +56,10 @@ class ServiceCollectionExtensions
             $configuration($builder);
         }
 
-        $service->addSingleton(Authentication::class, fn() => $builder->build());
+        $services->addSingleton(Authentication::class, fn() => $builder->build());
     }
 
-    public static function addAuthorisation(IServiceCollection $service, Closure $configuration = null)
+    public static function addAuthorisation(IServiceCollection $services, Closure $configuration = null)
     {
         $options = new AuthorizationOptions();
         $options->addPolicy("Authentication", fn($policy) => $policy->requireAuthentication());
@@ -80,20 +69,20 @@ class ServiceCollectionExtensions
             $configuration($options);
         }
 
-        $service->addSingleton(Authorization::class, fn() => new Authorization($options));
+        $services->addSingleton(Authorization::class, fn() => new Authorization($options));
     }
 
-    public static function addDbConnection(IServiceCollection $service, string $connection)
+    public static function addDbConnection(IServiceCollection $services, string $connection)
     {
-        $service->addSingleton(DbConnection::class, fn() => new DbConnection($connection));
+        $services->addSingleton(DbConnection::class, fn() => new DbConnection($connection));
     }
 
-    public static function addView(IServiceCollection $service, string $directory)
+    public static function addView(IServiceCollection $services, string $directory)
     {
-        $service->addSingleton(ViewManager::class, fn() => new ViewManager($directory));
+        $services->addSingleton(ViewManager::class, fn() => new ViewManager($directory));
     }
     
-    public static function addMvc(IServiceCollection $service, Closure $configuration = null)
+    public static function addMvc(IServiceCollection $services, Closure $configuration = null)
     {
         $options = new MvcOptions();
 
@@ -103,15 +92,12 @@ class ServiceCollectionExtensions
         }
         
         $viewDirectory  = $options->getViewDirectory();
-        $service->addView($viewDirectory);
-        $service->addHttpContext();
-
-        $service->addSingleton(MvcOptions::class, $options);
-
-        $service->addSingleton(RouteBuilder::class, fn($provider) => new RouteBuilder($provider, new MvcRouteHandler($provider)));
+        $services->addView($viewDirectory);
+        $services->addSingleton(MvcOptions::class, $options);
+        $services->addSingleton(RouteBuilder::class, fn($provider) => new RouteBuilder($provider, new MvcRouteHandler($provider)));
     }
 
-    public static function addEntityContext(IServiceCollection $service, string $entityConext, Closure $configuration = null)
+    public static function addEntityContext(IServiceCollection $services, string $entityConext, Closure $configuration = null)
     {
         $entityOptions = new EntityOptions;
 
@@ -120,34 +106,34 @@ class ServiceCollectionExtensions
             $configuration($entityOptions);
         }
         
-        $service->addSingleton($entityConext, fn() => new $entityConext($entityOptions));
-        $service->addSingleton(EntityContext::class, fn($provider) => $provider->getService($entityConext));
+        $services->addSingleton($entityConext, fn() => new $entityConext($entityOptions));
+        $services->addSingleton(EntityContext::class, fn($provider) => $provider->getService($entityConext));
     }
 
-    public static function addIdentity(IServiceCollection $service, string $userType = User::class, string $roleType = Role::class, Closure $configuration = null)
+    public static function addIdentity(IServiceCollection $services, string $userType = User::class, string $roleType = Role::class, Closure $configuration = null)
     {
-        self::addAuthentication($service, $configuration);
+        self::addAuthentication($services, $configuration);
 
-        $service->addSingleton(IdentityContext::class, function($provider) use ($userType, $roleType) : IdentityContext
+        $services->addSingleton(IdentityContext::class, function($provider) use ($userType, $roleType) : IdentityContext
         {
             $httpContext = $provider->getService(HttpContext::class);
             $entityContext = $provider->getService(EntityContext::class);
             return new IdentityContext($httpContext, $entityContext, $userType, $roleType);
         });
 
-        $service->addSingleton(IdentityManager::class, function($provider) : IdentityManager
+        $services->addSingleton(IdentityManager::class, function($provider) : IdentityManager
         {
             $identityContext = $provider->getService(IdentityContext::class);
             return new IdentityManager($identityContext);
         });
 
-        $service->addSingleton(UserManager::class, function($provider) : UserManager
+        $services->addSingleton(UserManager::class, function($provider) : UserManager
         {
             $identityContext = $provider->getService(IdentityContext::class);
             return new UserManager($identityContext);
         });
         
-        $service->addSingleton(RoleManager::class, function($provider) : RoleManager
+        $services->addSingleton(RoleManager::class, function($provider) : RoleManager
         {
             $identityContext = $provider->getService(IdentityContext::class);
             return new RoleManager($identityContext);

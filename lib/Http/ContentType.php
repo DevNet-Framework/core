37a263c98d72c31f26dfc3c6fee57f01d7c9ab30
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-12-20 15:14:35 -0500

    add ContentType class

---
+++ lib/Http/ContentType.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\Web\Http;
+
+class ContentType
+{
+    public static $Text = 'text/plain';
+    public static $Html = 'text/html';
+    public static $Png  = 'image/png';
+    public static $Pdf  = 'application/pdf';
+    public static $Json = 'application/json';
+    public static $Form = 'application/x-www-form-urlencoded';
+    public static $Data = 'multipart/form-data';
+}

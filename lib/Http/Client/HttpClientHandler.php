1a8a00ea7792855ab72916ec743402e438e218e5
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-12-21 05:27:10 -0500

    add HttpClientHandler

---
+++ lib/Http/Client/HttpClientHandler.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\Web\Http\Client;
+
+use DevNet\System\Async\Tasks\Task;
+use DevNet\Web\Http\Client\Internal\HttpRequestRawBuilder;
+use DevNet\Web\Http\Client\Internal\HttpResponseParser;
+use DevNet\Web\Http\HttpException;
+use DevNet\Web\Http\HttpRequest;
+use DevNet\Web\Http\HttpResponse;
+
+abstract class HttpClientHandler
+{
+    protected HttpClientOptions $Options;
+
+    abstract public function __construct(HttpClientOptions $options);
+
+    public function send(HttpRequest $request): HttpResponse
+    {
+        return $this->sendAsync($request)->getAwaiter()->getResult();
+    }
+
+    public function sendAsync(HttpRequest $request): Task
+    {
+        if (!$request->Headers->contains('host')) {
+            $request->Headers->add('host', $request->Uri->Host);
+        }
+
+        $address    = $request->Uri->Host . ":" . $request->Uri->Port;
+        $requestRaw = HttpRequestRawBuilder::build($request);
+        
+        return Task::Run(function () use ($address, $requestRaw) {
+            $source = stream_socket_client($address, $errorCode, $errorMessage);
+            if (!$source) {
+                throw new HttpException($errorMessage, $errorCode);
+            }
+
+            fwrite($source, $requestRaw);
+            stream_set_blocking($source, 0);
+
+            if ($this->Options->Timeout) {
+                stream_set_timeout($source, $this->Options->Timeout);
+            }
+
+            // get Response header
+            $responseHeader = '';
+            do {
+                $responseHeader .= fgets($source);
+                $info = stream_get_meta_data($source);
+                if ($info['timed_out']) {
+                    fclose($source);
+                    throw new \Exception('HttpClient Connection timed out!');
+                }
+                yield;
+            } while (strpos($responseHeader, "\r\n\r\n") === false);
+
+            // get Response Body
+            $responseBody = '';
+            while (!feof($source)) {
+                $responseBody .= fread($source, 1024);
+                yield;
+            }
+
+            fclose($source);
+            $response = HttpResponseParser::parse($responseHeader, $responseBody);
+            return $response;
+        });
+    }
+}

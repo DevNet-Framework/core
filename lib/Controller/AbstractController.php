c35232d223bb5fe59699187a9baa24a782d0ceda
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-07-23 04:46:35 -0400

    modify AbstractController replace abstract methods with implemented methods check if the filter added exists and implements IMiddleware interface

--- lib/Controller/AbstractController.php
+++ lib/Controller/AbstractController.php
@@ -9,32 +9,91 @@
 
 namespace DevNet\Web\Controller;
 
+use DevNet\System\Exceptions\ArgumentException;
+use DevNet\System\Exceptions\ClassException;
+use DevNet\Web\Controller\Features\HtmlHelper;
+use DevNet\Web\Controller\Features\UrlHelper;
 use DevNet\Web\Controller\Results\ContentResult;
 use DevNet\Web\Controller\Results\ForbidResult;
 use DevNet\Web\Controller\Results\JsonResult;
 use DevNet\Web\Controller\Results\RedirectResult;
 use DevNet\Web\Controller\Results\ViewResult;
 use DevNet\Web\Http\HttpContext;
+use DevNet\Web\Middleware\IMiddleware;
+use DevNet\Web\View\ViewManager;
 
 abstract class AbstractController
 {
     public HttpContext $HttpContext;
     public ActionDescriptor $ActionDescriptor;
     public array $ActionFilters = [];
+    protected array $ViewData = [];
 
     public function filter(string $target, string $filter, array $options = []): void
     {
+        $interfaces = class_implements($filter);
+        if ($interfaces === false) {
+            throw new ClassException("Could not find middleware {$filter}");
+        }
+
+        if (!in_array(IMiddleware::class, $interfaces)) {
+            throw new ClassException("{$filter} must implements IMiddleware inteface");
+        }
+
         $target = strtolower($target);
         $this->ActionFilters[$target][] = [$filter, $options];
     }
 
-    abstract public function view($parameter, object $model = null): ViewResult;
+    public function view($parameter = null, object $model = null): ViewResult
+    {
+        if (!$parameter) {
+            $viewName = null;
+        } else if (is_string($parameter)) {
+            $viewName = $parameter;
+        } else if (is_object($parameter)) {
+            $viewName = null;
+            $model    = $parameter;
+        } else {
+            throw new \Exception("Invalide argument type, parameter 1 must be string or object");
+        }
 
-    abstract public function content(string $content, string $contentType): ContentResult;
+        if (!$viewName) {
+            $prefix         = $this->HttpContext->RouteContext->RouteData->Values['prefix'];
+            $controllerName = $this->ActionDescriptor->ControllerName;
+            $controllerName = str_replace('Controller', '', $this->ActionDescriptor->ControllerName);
+            $actionName     = $this->ActionDescriptor->ActionName;
+            $viewName       = ucwords($prefix . $controllerName, '/') . '/' . $actionName;
+        }
 
-    abstract public function json(array $data): JsonResult;
+        $view = $this->HttpContext->RequestServices->getService(ViewManager::class);
+        $view->setData($this->ViewData);
+        $view->inject('Html', new HtmlHelper($this->HttpContext));
+        $view->inject('Url', new UrlHelper($this->HttpContext));
+        return new ViewResult($view($viewName, $model), 200);
+    }
 
-    abstract public function redirect(string $path): RedirectResult;
+    public function content(string $content, string $contentType = 'text/plain'): ContentResult
+    {
+        return new ContentResult($content, $contentType);
+    }
 
-    abstract public function forbidResult(): ForbidResult;
+    public function json($data, $statusCode = 200): JsonResult
+    {
+        if (!is_array($data) && !is_object($data)) {
+            $class = get_class($this);
+            throw new ArgumentException("Argument 1 passed to {$class}::json() must be of the type object | array.");
+        }
+
+        return new JsonResult($data, $statusCode);
+    }
+
+    public function redirect(string $path): RedirectResult
+    {
+        return new RedirectResult($path);
+    }
+
+    public function forbidResult(): ForbidResult
+    {
+        return new ForbidResult();
+    }
 }

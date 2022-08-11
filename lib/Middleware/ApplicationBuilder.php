dda5e8905a6acbaa52e7fedf1c2dc507268104b8
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-08-11 13:07:12 -0400

    update ApplicationBuilder remove pipe method use RequestDelegate directly instead

--- lib/Middleware/ApplicationBuilder.php
+++ lib/Middleware/ApplicationBuilder.php
@@ -9,7 +9,6 @@
 
 namespace DevNet\Web\Middleware;
 
-use DevNet\System\Async\AsyncFunction;
 use DevNet\System\Async\Tasks\Task;
 use DevNet\System\Dependency\IServiceProvider;
 use DevNet\System\Exceptions\PropertyException;
@@ -53,14 +52,6 @@ class ApplicationBuilder implements IApplicationBuilder
         $this->middlewares[] = new MiddlewareDelegate($middleware);
     }
 
-    public function pipe(callable $middleware, $next): RequestDelegate
-    {
-        return new RequestDelegate(function (HttpContext $context) use ($middleware, $next) {
-            $middlewareAsync = new AsyncFunction($middleware);
-            return $middlewareAsync($context, $next);
-        });
-    }
-
     public function Build(): RequestDelegate
     {
         $app = new RequestDelegate(function (HttpContext $context): Task {
@@ -75,7 +66,9 @@ class ApplicationBuilder implements IApplicationBuilder
         });
 
         foreach (array_reverse($this->middlewares) as $middleware) {
-            $app = $this->pipe($middleware, $app);
+            $app = new RequestDelegate(function (HttpContext $context) use ($middleware, $app) {
+                return $middleware($context, $app);
+            });
         }
 
         return $app;

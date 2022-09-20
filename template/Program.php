c003e387edcfe3c897774699e182ddcf87aeb9cb
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-09-20 15:11:37 -0400

    add web template

---
+++ template/Program.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application;
+
+use DevNet\Web\Extensions\ApplicationBuilderExtensions;
+use DevNet\Web\Extensions\ServiceCollectionExtensions;
+use DevNet\Web\Hosting\WebHost;
+
+class Program
+{
+    public static function main(array $args = [])
+    {
+        $builder = WebHost::createDefaultBuilder($args);
+        $configuration = $builder->ConfigBuilder->build();
+
+        $builder->configureServices(function ($services) {
+            // services
+        });
+
+        $host = $builder->build();
+
+        $host->start(function ($app) use ($configuration) {
+            if ($configuration->getValue('environment') == 'development') {
+                $app->UseExceptionHandler();
+            } else {
+                $app->UseExceptionHandler("/home/error");
+            }
+
+            $app->useRouter();
+            // middlewares
+            $app->useEndpoint(function ($routes) {
+                $routes->mapGet("/", fn ($context) => $context->Response->writeAsync("Hello World!"));
+            });
+        });
+    }
+}

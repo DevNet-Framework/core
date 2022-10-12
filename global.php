f6a3facf97dbee66984aa9ced4e1b3eb963e0ba8
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-10-12 16:15:14 -0400

    update global.php register WebTemplateProvider

--- global.php
+++ global.php
@@ -6,3 +6,8 @@
  * @license     MIT License. For full license information see LICENSE file in the project root.
  * @link        https://github.com/DevNet-Framework
  */
+
+use DevNet\Cli\Templating\TemplateRegistry;
+use DevNet\Web\Tools\WebTemplateProvider;
+
+TemplateRegistry::register('web', WebTemplateProvider::class);

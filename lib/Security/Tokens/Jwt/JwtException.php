a9d07a927e562deef0db978a512f787ed35ce7b9
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-12-10 05:17:05 -0500

    add JwtException

---
+++ lib/Security/Tokens/Jwt/JwtException.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\Web\Security\Tokens\Jwt;
+
+use Exception;
+
+class JwtException extends Exception
+{
+}

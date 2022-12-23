1829310409394eacdd6c94b0cbeb7548e54f7489
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-12-23 18:26:32 -0500

    add JwtHeader

---
+++ lib/Security/Tokens/Jwt/JwtHeader.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\Web\Security\Tokens\Jwt;
+
+use DevNet\Web\Security\Tokens\Base64UrlEncoder;
+
+class JwtHeader
+{
+    public string $Alg;
+    public string $Typ;
+    public ?string $Cty;
+
+    public function __construct(string $algorithm = 'HS256', string $type = 'JWT', ?string $cententType = null)
+    {
+        $this->Alg = strtoupper($algorithm);
+        $this->Typ = strtoupper($type);
+        $this->Cty = $cententType;
+    }
+
+    public function toJson(): string
+    {
+        $header = [
+            'alg' => $this->Alg,
+            'typ' => $this->Typ
+        ];
+
+        if ($this->Cty) {
+            $header['cty'] = $this->Cty;
+        }
+
+        return json_encode($header);
+    }
+
+    public function encode(): string
+    {
+        return Base64UrlEncoder::encode($this->toJson());
+    }
+}

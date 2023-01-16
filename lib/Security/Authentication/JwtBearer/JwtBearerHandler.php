d6435210b2f23439f806639cfa58a3a5aeeac8e1
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-01-15 23:11:02 -0500

    add JwtBearerHandler

---
+++ lib/Security/Authentication/JwtBearer/JwtBearerHandler.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\Web\Security\Authentication\JwtBearer;
+
+use DevNet\System\ObjectTrait;
+use DevNet\Web\Http\HttpContext;
+use DevNet\Web\Security\Authentication\AuthenticationResult;
+use DevNet\Web\Security\Authentication\IAuthenticationHandler;
+use DevNet\Web\Security\Claims\ClaimsPrincipal;
+use DevNet\Web\Security\Tokens\Jwt\JwtSecurityTokenHandler;
+use Exception;
+
+class JwtBearerHandler implements IAuthenticationHandler
+{
+    use ObjectTrait;
+
+    private HttpContext $HttpContext;
+    private JwtBearerOptions $options;
+    private JwtSecurityTokenHandler $handler;
+
+    public function __construct(HttpContext $httpContext, JwtBearerOptions $options)
+    {
+        $this->httpContext = $httpContext;
+        $this->options = $options;
+        $this->handler = new JwtSecurityTokenHandler();
+    }
+
+    public function get_Options(): JwtBearerOptions
+    {
+        return $this->options;
+    }
+
+    public function readToken(): string
+    {
+        $headers = $this->httpContext->Request->Headers->getValues('Authorization');
+        if (!$headers) {
+            throw new Exception("The request is missing the authorization header!");
+        }
+
+        if (!preg_match("/^Bearer\s+(.*)$/", $headers[0], $matches)) {
+            throw new Exception("Incorrect authentication header scheme!");
+        }
+
+        return $matches[1];
+    }
+
+    public function authenticate(): AuthenticationResult
+    {
+        try {
+            $token = $this->readToken();
+            $jwtToken = $this->handler->validateToken($token, $this->SecurityKey, $this->Options->Issuer, $this->Options->Audiance);
+            return new AuthenticationResult(new ClaimsPrincipal($jwtToken->Payload->Claims));
+        } catch (\Throwable $exception) {
+            return new AuthenticationResult($exception);
+        }
+    }
+}

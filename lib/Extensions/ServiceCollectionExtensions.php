3c8b1147650d3dba1c9a1600c76b412eef88453d
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-05-10 17:42:17 -0400

    update ServiceCollectionExtensions remove ContextType from addEntityContext() method as parameter and get it from EntityOptions::ContextType property

--- lib/Extensions/ServiceCollectionExtensions.php
+++ lib/Extensions/ServiceCollectionExtensions.php
@@ -108,13 +108,14 @@ class ServiceCollectionExtensions
         $services->addSingleton(DbConnection::class, fn (): DbConnection => new DbConnection($datasource, $username, $password));
     }
 
-    public static function addEntityContext(IServiceCollection $services, string $contextType, Closure $configuration = null)
+    public static function addEntityContext(IServiceCollection $services, Closure $configuration = null)
     {
         $options = new EntityOptions();
         if ($configuration) {
             $configuration($options);
         }
 
+        $contextType = $options->ContextType;
         $services->addSingleton($contextType, fn (): EntityContext => new $contextType($options));
         $services->addSingleton(EntityContext::class, fn ($provider): EntityContext => $provider->getService($contextType));
     }

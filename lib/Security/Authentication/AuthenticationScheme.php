d96d42c95e0013e903b08b737e34d845f20104b7
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-08-11 13:16:02 -0400

    add AuthenticationScheme class

---
+++ lib/Security/Authentication/AuthenticationScheme.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\Web\Security\Authentication;
+
+class AuthenticationScheme
+{
+    public const CookieSession = 'CookieSession';
+    public const JwtBearer     = 'JwtBearer';
+}

418c96c7c35d794ecba2b04568dc4fca6255e68e
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-08-26 11:57:38 -0400

    update Authorize - Allow anonymous authentication - improve work logic

--- lib/Action/Filters/Authorize.php
+++ lib/Action/Filters/Authorize.php
@@ -14,9 +14,12 @@ use DevNet\Web\Action\ActionContext;
 use DevNet\Web\Action\ActionDelegate;
 use DevNet\Web\Action\IActionFilter;
 use DevNet\Web\Security\Authentication\AuthenticationException;
+use DevNet\Web\Security\Authorization\AuthenticationRequirement;
+use DevNet\Web\Security\Authorization\Authorization;
 use DevNet\Web\Security\Authorization\AuthorizationContext;
 use DevNet\Web\Security\Authorization\AuthorizationException;
 use DevNet\Web\Security\Authorization\RolesRequirement;
+use DevNet\Web\Security\Claims\ClaimsIdentity;
 use Attribute;
 
 #[Attribute]
@@ -35,34 +38,30 @@ class Authorize implements IActionFilter
 
     public function __invoke(ActionContext $context, ActionDelegate $next): Task
     {
-        // Allow anonymous authorization
+        // Allow anonymous authentication
         if ($this->scheme == "Anonymous") {
             return $next($context);
         }
 
         $user = $context->HttpContext->User;
-
-        if ($this->scheme) {
-            $identity = $user->Identities[$this->scheme] ?? null;
-            if (!$identity || !$identity->isAuthenticated()) {
-                throw new AuthenticationException("The current user is not authenticated with the selected authentication scheme '{$this->scheme}'!", 401);
-            }
+        if ($this->scheme != $user->AuthenticationType) {
+            // set unauthenticated user for this scheme and only in this filter.
+            $user = new ClaimsIdentity();
         }
 
-        if (!$user->isAuthenticated()) {
-            throw new AuthenticationException("The current user is not authenticated!", 401);
+        $authorization = $context->HttpContext->RequestServices->getService(Authorization::class);
+        if (!$authorization) {
+            throw new AuthorizationException("Unable to get the Authorization service, make sure if it's already registered!");
         }
 
-        if ($this->policy) {
-            $authorization = $context->HttpContext->RequestServices->getService(Authorization::class);
-            if (!$authorization) {
-                throw new AuthorizationException("Unable to get the Authorization service, make sure if it's already registered!");
+        $result = $authorization->authorize($user, $this->policy);
+        if (!$result->IsSucceeded) {
+            $requirement = $result->failedRequirements[0] ?? null;
+            if ($requirement instanceof AuthenticationRequirement) {
+                throw new AuthenticationException("The current user is not authenticated!", 401);
             }
 
-            $result = $authorization->Authorize($user, $this->policy);
-            if (!$result->isSucceeded()) {
-                throw new AuthorizationException("Current user claims do not meet the authorization policy required!", 403);
-            }
+            throw new AuthorizationException("The current user does not meet the required authorization policy!", 403);
         }
 
         if ($this->roles) {
@@ -70,8 +69,8 @@ class Authorize implements IActionFilter
             $autorizeContext = new AuthorizationContext([$requirement], $user);
             $requirement->getHandler()->handle($autorizeContext)->wait();
             $result = $autorizeContext->getResult();
-            if (!$result->isSucceeded()) {
-                throw new AuthorizationException("Current user claims do not meet the authorization roles required!", 403);
+            if (!$result->IsSucceeded) {
+                throw new AuthorizationException("The current user does not meet the required authorization roles!", 403);
             }
         }
 

e9bf2ce4449f3a96782d98ee9eea6e9f868b0e1e
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-11-02 18:12:58 -0400

    bind the model with the request query string

--- lib/Endpoint/Binder/ModelBinder.php
+++ lib/Endpoint/Binder/ModelBinder.php
@@ -9,6 +9,8 @@
 
 namespace DevNet\Web\Endpoint\Binder;
 
+use ReflectionMethod;
+
 class ModelBinder implements IModelBinder
 {
     public function bind(BindingContext $bindingContext)
@@ -17,20 +19,35 @@ class ModelBinder implements IModelBinder
 
         if (class_exists($type)) {
             $model = new $type();
+            $query  = $bindingContext->ActionContext->HttpContext->Request->Query;
+            foreach ($query as $name => $value) {
+                if (property_exists($model, $name)) {
+                    $method = new ReflectionMethod($model, $name);
+                    if ($method->isPublic()) {
+                        $model->$name = $value;
+                    }
+                }
+            }
+            
             $form  = $bindingContext->ActionContext->HttpContext->Request->Form;
-
-            foreach ($form->Fields as $key => $value) {
-                if (property_exists($model, $key)) {
-                    $model->$key = $value;
+            foreach ($form->Fields as $name => $value) {
+                if (property_exists($model, $name)) {
+                    $method = new ReflectionMethod($model, $name);
+                    if ($method->isPublic()) {
+                        $model->$name = $value;
+                    }
                 }
             }
 
             foreach ($form->Files as $name => $upload) {
                 if (property_exists($model, $name)) {
-                    if (count($upload) == 1) {
-                        $model->$name = $upload[0];
-                    } else {
-                        $model->$key = $upload;
+                    $method = new ReflectionMethod($model, $name);
+                    if ($method->isPublic()) {
+                        if (count($upload) == 1) {
+                            $model->$name = $upload[0];
+                        } else {
+                            $model->$name = $upload;
+                        }
                     }
                 }
             }
